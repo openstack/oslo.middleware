16520d9842fb44d46b3a556f7657620fcb342792
Joe Gordon <joe.gordon0@gmail.com>
2013-12-20 18:11:50 -0800

    Use hacking import_exceptions for gettextutils._
    Instead of using # noqa every time gettextutils._ is used, add an
    exception in tox.ini
    
    Change-Id: Iafa8509a38596aac791bd809283f3bd75c638a60

--- openstack/common/middleware/sizelimit.py
+++ openstack/common/middleware/sizelimit.py
@@ -21,7 +21,7 @@ from oslo.config import cfg
 import webob.dec
 import webob.exc
 
-from openstack.common.gettextutils import _  # noqa
+from openstack.common.gettextutils import _
 from openstack.common.middleware import base
 
 
