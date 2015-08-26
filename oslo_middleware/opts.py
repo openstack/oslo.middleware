ea893e8ba417aeb13455acfda30b8337159b8536
Doug Hellmann <doug@doughellmann.com>
2015-08-26 17:19:12 +0000

    Split option discovery function by middleware
    Create separate option discovery functions for each piece of middleware,
    so an application that only uses one piece can include only those
    options in its sample configuration file.
    
    Test the results with some unit tests that simply ensure the functions
    do not raise exceptions, and by adding configuration options to the
    documentation for the library, using the new integration of oslo.config
    with sphinx.
    
    Change-Id: I4c777cd70c063441f430c48ab1f9c9cac2c1fc75

--- oslo_middleware/opts.py
+++ oslo_middleware/opts.py
@@ -15,10 +15,14 @@
 
 __all__ = [
     'list_opts',
+    'list_opts_sizelimit',
+    'list_opts_ssl',
+    'list_opts_cors'
 ]
 
 
 import copy
+import itertools
 
 from oslo_middleware import cors
 from oslo_middleware import sizelimit
@@ -26,7 +30,35 @@ from oslo_middleware import ssl
 
 
 def list_opts():
-    """Return a list of oslo.config options available in the library.
+    """Return a list of oslo.config options for ALL of the middleware classes.
+
+    The returned list includes all oslo.config options which may be registered
+    at runtime by the library.
+
+    Each element of the list is a tuple. The first element is the name of the
+    group under which the list of elements in the second element will be
+    registered. A group name of None corresponds to the [DEFAULT] group in
+    config files.
+
+    This function is also discoverable via the 'oslo.concurrency' entry point
+    under the 'oslo.config.opts' namespace.
+
+    The purpose of this is to allow tools like the Oslo sample config file
+    generator to discover the options exposed to users by this library.
+
+    :returns: a list of (group_name, opts) tuples
+    """
+    return list(
+        itertools.chain(
+            list_opts_sizelimit(),
+            list_opts_ssl(),
+            list_opts_cors(),
+        )
+    )
+
+
+def list_opts_sizelimit():
+    """Return a list of oslo.config options for the sizelimit middleware.
 
     The returned list includes all oslo.config options which may be registered
     at runtime by the library.
@@ -45,7 +77,54 @@ def list_opts():
     :returns: a list of (group_name, opts) tuples
     """
     return [
-        ('oslo_middleware', copy.deepcopy(sizelimit._opts + ssl.OPTS)),
+        ('oslo_middleware', copy.deepcopy(sizelimit._opts)),
+    ]
+
+
+def list_opts_ssl():
+    """Return a list of oslo.config options for the sizelimit middleware.
+
+    The returned list includes all oslo.config options which may be registered
+    at runtime by the library.
+
+    Each element of the list is a tuple. The first element is the name of the
+    group under which the list of elements in the second element will be
+    registered. A group name of None corresponds to the [DEFAULT] group in
+    config files.
+
+    This function is also discoverable via the 'oslo.concurrency' entry point
+    under the 'oslo.config.opts' namespace.
+
+    The purpose of this is to allow tools like the Oslo sample config file
+    generator to discover the options exposed to users by this library.
+
+    :returns: a list of (group_name, opts) tuples
+    """
+    return [
+        ('oslo_middleware', copy.deepcopy(ssl.OPTS)),
+    ]
+
+
+def list_opts_cors():
+    """Return a list of oslo.config options for the cors middleware.
+
+    The returned list includes all oslo.config options which may be registered
+    at runtime by the library.
+
+    Each element of the list is a tuple. The first element is the name of the
+    group under which the list of elements in the second element will be
+    registered. A group name of None corresponds to the [DEFAULT] group in
+    config files.
+
+    This function is also discoverable via the 'oslo.concurrency' entry point
+    under the 'oslo.config.opts' namespace.
+
+    The purpose of this is to allow tools like the Oslo sample config file
+    generator to discover the options exposed to users by this library.
+
+    :returns: a list of (group_name, opts) tuples
+    """
+    return [
         ('cors', copy.deepcopy(cors.CORS_OPTS)),
         ('cors.subdomain', copy.deepcopy(cors.CORS_OPTS))
     ]
