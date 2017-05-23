0e3dbba0d9d373fbfa9e28897cd33377e2491d32
Sean Dague <sean@dague.net>
2017-05-23 07:37:10 -0400

    Set global_request_id if passed in as X-OpenStack-Request-ID.
    If an inbound header that validates against the regex ``req-$uuid`` is
    passed in, we'll set it where oslo.context could later read it. This
    creates a mechanism for parents to pass their request-id to children.
    
    Change-Id: I5c5268e8680392395fbe04efe0124575ad8db468

--- oslo_middleware/tests/test_request_id.py
+++ oslo_middleware/tests/test_request_id.py
@@ -13,6 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import uuid
 
 from oslotest import base as test_base
 from testtools import matchers
@@ -63,3 +64,43 @@ class RequestIdTest(test_base.BaseTestCase):
 
         self.assertEqual(res.headers.get("x-compute-req-id"), res_req_id)
         self.assertEqual(res.headers.get("x-silly-id"), res_req_id)
+
+    def test_global_request_id_set(self):
+        """Test that global request_id is set."""
+        @webob.dec.wsgify
+        def application(req):
+            return req.environ[request_id.GLOBAL_REQ_ID]
+
+        global_req = "req-%s" % uuid.uuid4()
+        app = request_id.RequestId(application)
+        req = webob.Request.blank(
+            '/test',
+            headers={"X-OpenStack-Request-ID": global_req})
+        res = req.get_response(app)
+        res_req_id = res.headers.get(request_id.HTTP_RESP_HEADER_REQUEST_ID)
+        if isinstance(res_req_id, bytes):
+            res_req_id = res_req_id.decode('utf-8')
+        # global-request-id in request environ is returned as response body
+        self.assertEqual(res.body.decode('utf-8'), global_req)
+        self.assertNotEqual(res.body.decode('utf-8'), res_req_id)
+
+    def test_global_request_id_drop(self):
+        """Test that bad format ids are dropped.
+
+        This ensures that badly formatted ids are dropped entirely.
+        """
+        @webob.dec.wsgify
+        def application(req):
+            return req.environ.get(request_id.GLOBAL_REQ_ID)
+
+        global_req = "req-%s-bad" % uuid.uuid4()
+        app = request_id.RequestId(application)
+        req = webob.Request.blank(
+            '/test',
+            headers={"X-OpenStack-Request-ID": global_req})
+        res = req.get_response(app)
+        res_req_id = res.headers.get(request_id.HTTP_RESP_HEADER_REQUEST_ID)
+        if isinstance(res_req_id, bytes):
+            res_req_id = res_req_id.decode('utf-8')
+        # global-request-id in request environ is returned as response body
+        self.assertEqual(res.body.decode('utf-8'), '')
